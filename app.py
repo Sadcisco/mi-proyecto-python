c340aeefbf02a98ce660fabd9c4c37619a7e5cc8
Tu Nombre <tuemail@example.com>
2025-03-24 14:06:32 -0300

    Resuelto conflicto entre ramas

--- app.py
+++ app.py
@@ -1,9 +1,15 @@
 from flask import Flask
 app = Flask(__name__)
 
+@app.route('/nueva-ruta')
+def nueva_ruta():
+    return '¡Esta es una nueva ruta!'
+
 @app.route('/')
 def hola_mundo():
     return 'Hola Mundo!'
 
+
 if __name__ == '__main__':
     app.run(port=5000)
+
